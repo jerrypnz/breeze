f84c6ceb8ca8915d3454bb7e910ea6231370346d
Jerry Peng <pr2jerry@gmail.com>
2013-07-15 23:45:01 +0800

    Add Server header to test_http_server

--- src/tests/test_http_server.c
+++ src/tests/test_http_server.c
@@ -46,6 +46,7 @@ int foobar_handler(request_t *req, response_t *resp, handler_ctx_t *ctx) {
     resp->content_length = len;
     resp->connection = CONN_CLOSE;
     response_set_header(resp, "Content-Type", "text/html");
+    response_set_header(resp, "Server", "breeze/0.1.0");
 
     response_write(resp, response, len, bar_handler);
     return 0;
